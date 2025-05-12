7e578b3a4e14cb07f2dedac161a613bfc97490d3
Erik Svensson <arizon.es@gmail.com>
2025-05-13 00:14:06 +0200

    add fish settings to git

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting " 
+       .air.    _
+        \":\"  ._|_|_.
+         : ._|_|_|_|
+      ___:_|_|_|_|_|     |\"\\ /\"|
+    ,'              `.    \   /
+    |  O              \___/  |
+  ~^~^~^~^~^~^~^~^~^~^~^~^~^~^~"
+         oh-my-posh init fish --config ~/.cache/oh-my-posh/arizon.omp.json | source
+    
+end
